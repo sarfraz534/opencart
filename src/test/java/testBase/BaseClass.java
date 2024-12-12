29e57aa5065e269216ca18c94a670cab2131c5c9
Sarfraz Ahmad <hieahmad534@gmail.com>
2024-12-12 16:06:44 +0530

    added grid and updated config.properties file and testbase--> base class

--- src/test/java/testBase/BaseClass.java
+++ src/test/java/testBase/BaseClass.java
@@ -3,6 +3,7 @@ package testBase;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.util.Date;
@@ -10,10 +11,13 @@ import java.util.Properties;
 
 import org.apache.commons.lang3.RandomStringUtils;
 import org.openqa.selenium.OutputType;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
@@ -37,12 +41,36 @@ public class BaseClass {
 		 
 		logger=LogManager.getLogger(this.getClass());//Log4j
 		
+		if(p.getProperty("execution_env").equalsIgnoreCase("remote")) {
+			
+			DesiredCapabilities capabilities=new DesiredCapabilities();
+			//OS
+			if(os.equalsIgnoreCase("windows")) {
+				capabilities.setPlatform(Platform.WIN10);
+			}
+			else if(os.equalsIgnoreCase("mac")) {
+				capabilities.setPlatform(Platform.MAC);
+			}
+			else System.out.println("no matching os");
+			
+			//browser
+			switch(br.toLowerCase()) {
+			case "chrome":capabilities.setBrowserName("chrome"); break;
+			case "edge": capabilities.setBrowserName("MicrosoftEdge"); break;
+			default : System.out.println("no matching browser");
+			}
+			driver=new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capabilities);
+		}
+		
+		
+		if(p.getProperty("execution_env").equalsIgnoreCase("local")) {
 		switch(br.toLowerCase())
 		{
 		case "chrome": driver=new ChromeDriver(); break;
 		case "edge": driver= new EdgeDriver(); break;
 		default :System.out.println("No matching browser.."); return;
 		
+		}
 		}
 		
 		driver.manage().deleteAllCookies();
